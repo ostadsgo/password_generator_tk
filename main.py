1f11813eb8bf8f7d0cb148286f15fd5db9d3d64f
Saeed Gholami <saeed.ghollami@gmail.com>
2023-12-10 20:06:13 +0330

    Disable password entry, Add colors to app

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
-from random import choices,  shuffle
+from random import choices, shuffle
 import tkinter as tk
 from tkinter import ttk
+
+# Exceptions:
+#  - if user add words insted of number in the password_len entry
+#  - if user doens't choose any criteria
 
 
 class MainFrame(ttk.Frame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
+        ## Basic settings
+        # give focus to the frame
+        self.focus_set()
         self.master = master
-        #  random generated password
-        self.password_len = tk.IntVar()
-        self.password = tk.StringVar()
-        self.upper = tk.BooleanVar()
+        self.style = ttk.Style(self.master)
+
+        ## Variables
+        self.password_len = tk.IntVar(value=8)
+        self.password = tk.StringVar(value="Press Generate Password ...")
+        self.upper = tk.BooleanVar(value=True)
         self.lower = tk.BooleanVar()
         self.number = tk.BooleanVar()
         self.symbol = tk.BooleanVar()
+        # TODO: fix this using string module
         self.lowers = "".join(chr(i) for i in range(97, 123))
         self.uppers = "".join(ch.upper() for ch in self.lowers)
         self.numbers = "".join(chr(i) for i in range(48, 58))
         self.symbols = "@!#$%&^()*+"
+        self.criteria = [self.upper, self.lower, self.number, self.symbol]
+        # use join to this or maybe some claver way
         self.allchars = self.uppers + self.lowers + self.numbers + self.symbols
+        self.strength = tk.IntVar(value=0)
+        self.intens = ["weak", "normal", "strong", "powerfull"]
 
-        password_frame = ttk.Frame(self, relief=tk.SOLID, padding=(5, 10))
-        password_frame.pack(expand=True, fill=tk.BOTH, pady=(0, 10))
+        ## Widgets
+        password_frame = ttk.Frame(self, padding=(5, 10))
+        password_frame.pack(expand=True, fill=tk.BOTH)
 
-        password_entry = ttk.Entry(password_frame, width=50, textvariable=self.password)
+        password_entry = ttk.Entry(password_frame, textvariable=self.password)
         copy_button = ttk.Button(
             password_frame, text="Copy", command=self.copy_password
         )
         password_entry.pack(expand=True, fill=tk.BOTH, side=tk.LEFT)
-        copy_button.pack(expand=True, fill=tk.BOTH, padx=(10, 0))
+        copy_button.pack(expand=True, fill=tk.BOTH)
 
         # Settings for password
-        setting_frame = ttk.Frame(self, relief=tk.SOLID, padding=(5, 10))
-        setting_frame.pack(expand=True, fill=tk.BOTH)
+        setting_frame = ttk.Frame(self, relief=tk.SUNKEN, padding=(5, 10))
+        setting_frame.pack(expand=True, fill=tk.BOTH, pady=10)
 
         # settings
         uppercase_checkbutton = ttk.Checkbutton(
@@ -47,25 +62,53 @@ class MainFrame(ttk.Frame):
         symbol_checkbutton = ttk.Checkbutton(
             setting_frame, variable=self.symbol, text="Symbols"
         )
-        passwrod_len_label = ttk.Label(setting_frame, text="Password Length")
+        password_len_label = ttk.Label(setting_frame, text="Password Length")
         password_len_entry = ttk.Entry(setting_frame, textvariable=self.password_len)
         generate_password_button = ttk.Button(
             self, text="Generate Password", command=self.generate_password
         )
-        self.password_len.set(12)
+        progressbar = ttk.Progressbar(self, variable=self.strength)
+
+        ## Widgets config
+        password_entry.config( font="iosevka 16 bold", state="disabled")
+        password_len_entry["font"] = "iosevka 16 bold"
+
+        ## Pack the widgets
         uppercase_checkbutton.pack(expand=True, fill=tk.BOTH)
         lowercase_checkbutton.pack(expand=True, fill=tk.BOTH)
         symbol_checkbutton.pack(expand=True, fill=tk.BOTH)
         number_checkbutton.pack(expand=True, fill=tk.BOTH)
-        passwrod_len_label.pack(expand=True, fill=tk.BOTH)
+        password_len_label.pack(expand=True, fill=tk.BOTH)
         password_len_entry.pack(expand=True, fill=tk.BOTH)
         generate_password_button.pack(expand=True, fill=tk.BOTH)
+        progressbar.pack(expand=True, fill=tk.BOTH)
+
+        ## style
+        self.style.configure("TFrame", background="#B6BBC4")
+        self.style.configure("TLabel", font=("iosevka", 16))
+        self.style.configure("TCheckbutton", font=("iosevka", 16))
+        self.style.configure("TButton", font=("iosevka", 16), background="#DF826C")
+        self.style.map("TButton", background=[("pressed", "#DF826C")])
+        self.style.map(
+            "TEntry",
+            foreground=[("disabled", "black")],
+            fieldbackground=[("disabled", "#F2F1EB")],
+        )
+        print(password_entry.state())
+        print("Style password_entry", password_entry["style"])
+        print("Style layout for entry", self.style.layout("TEntry"))
+        print("Style options for entry", self.style.element_options("TEntry.field"))
+
+        ## binds
+        self.bind("<Return>", self.generate_password)
+        generate_password_button.bind("<Return>", self.generate_password)
 
     def copy_password(self):
         print("copy password to clipboard")
 
     def checked_number(self):
         """Return number of check boxses checked."""
+        # TODO: Can you make this in senior way.
         n = 0
         if self.upper.get():
             n += 1
@@ -77,7 +120,7 @@ class MainFrame(ttk.Frame):
             n += 1
         return n
 
-    def generate_password(self):
+    def generate_password(self, enve=None):
         password = []
         n, r = divmod(self.password_len.get(), self.checked_number())
         if self.upper.get():
@@ -92,14 +135,48 @@ class MainFrame(ttk.Frame):
         rem_password = choices(self.allchars, k=r)
         password.extend(rem_password)
         self.password.set("".join(password))
+        self.set_progress()
+
+    def get_password_status(self):
+        # TODO: Fix this code
+        # TODO: Use enum or dicit
+        checked_criteria = sum(item.get() for item in self.criteria)
+        if checked_criteria <= 1:
+            return "weak"
+        if checked_criteria == 2:
+            return "normal"
+        if checked_criteria == 3:
+            return "strong"
+        if checked_criteria == 4:
+            return "powerful"
+
+    def set_progress(self):
+        # TODO: Fix if chain enum instead of string or dict.
+        password_status = self.get_password_status()
+        if password_status == "weak":
+            self.strength.set(25)
+        elif password_status == "normal":
+            self.strength.set(50)
+        elif password_status == "strong":
+            self.strength.set(75)
+        elif password_status == "powerful":
+            self.strength.set(100)
+        else:
+            self.strength.set(0)
+
+    def password_strength(self):
+        pass
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Password Generator")
+        self.minsize(500, 300)
         mainframe = MainFrame(self, padding=(5, 10))
         mainframe.pack(expand=True, fill=tk.BOTH)
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
 
 
 if __name__ == "__main__":
